a69d08848dda9a07eee667c1f9373c67b0bb7b4e
androidtestap <androidtestap@gmail.com>
2014-08-04 17:41:00 +0530

    Proper Commenting

--- ContextAwareFramework/src/com/contextawareframework/backgroundservices/CAFService.java
+++ ContextAwareFramework/src/com/contextawareframework/backgroundservices/CAFService.java
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @File		CAFService        
+ * @Created:    20.07.2014
+ * @author:     Prasenjit
+ * Last Change: 24.07.2014 by Prasenjit
  */
 package com.contextawareframework.backgroundservices;
 
@@ -23,69 +28,75 @@ import android.util.Log;
 public class CAFService extends Service{
 
 	/** indicates how to behave if the service is killed */
-	   int mStartMode;
-	   /** interface for clients that bind */
-	   IBinder mBinder;     
-	   /** indicates whether onRebind should be used */
-	   boolean mAllowRebind;
-	   /** Tag for debugging */
-	   private final String TAG = "CAFService";
-	   private static boolean enableDebugging = false;
-	   
-	   /**
-	    * Method to start debugging, all log tags in this class will be printed
-	    */
-	   protected void setEnableDebugging(boolean value)
-	   {
-		   enableDebugging = value;
-	   }
-	   protected boolean getEnableDebugging()
-	   {
-		  return enableDebugging;		  
-	   }
-	   /** Called when the service is being created. */
-	   @Override
-	   public void onCreate() {
-		  if(enableDebugging)
-			  Log.d(TAG,"onCreate");
-	   }
+	int mStartMode;
 
-	   /** The service is starting, due to a call to startService() */
-	   @Override
-	   public int onStartCommand(Intent intent, int flags, int startId) {
-		   if(enableDebugging)
-			   Log.d(TAG,"onStartCommand");
-	       return mStartMode;
-	   }
+	/** interface for clients that bind */
+	IBinder mBinder;     
 
-	   /** A client is binding to the service with bindService() */
-	   @Override
-	   public IBinder onBind(Intent intent) {
-		   if(enableDebugging)
-			   Log.d(TAG,"onBind");
-	      return mBinder;
-	   }
+	/** indicates whether onRebind should be used */
+	boolean mAllowRebind;
 
-	   /** Called when all clients have unbound with unbindService() */
-	   @Override
-	   public boolean onUnbind(Intent intent) {
-		   if(enableDebugging)
-			   Log.d(TAG,"onUnbind");
-		   return mAllowRebind;
-	   }
+	/** Tag for debugging */
+	private final String TAG = "CAFService";
+	private static boolean enableDebugging = false;
 
-	   /** Called when a client is binding to the service with bindService()*/
-	   @Override
-	   public void onRebind(Intent intent) {
-		   if(enableDebugging)
-			   Log.d(TAG,"onRebind");
-	   }
+	/**
+	 * Method to start debugging, all log tags in this class will be printed
+	 */
+	protected void setEnableDebugging(boolean value)
+	{
+		enableDebugging = value;
+	}
+	/**
+	 * Method to stop debugging.
+	 */
+	protected boolean getEnableDebugging()
+	{
+		return enableDebugging;		  
+	}
+	/** Called when the service is being created. */
+	@Override
+	public void onCreate() {
+		if(enableDebugging)
+			Log.d(TAG,"onCreate");
+	}
 
-	   /** Called when The service is no longer used and is being destroyed */
-	   @Override
-	   public void onDestroy() {
-		   if(enableDebugging)
-			   Log.d(TAG,"onDestroy");
-	   }
-	
+	/** The service is starting, due to a call to startService() */
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
+		if(enableDebugging)
+			Log.d(TAG,"onStartCommand");
+		return mStartMode;
+	}
+
+	/** A client is binding to the service with bindService() */
+	@Override
+	public IBinder onBind(Intent intent) {
+		if(enableDebugging)
+			Log.d(TAG,"onBind");
+		return mBinder;
+	}
+
+	/** Called when all clients have unbound with unbindService() */
+	@Override
+	public boolean onUnbind(Intent intent) {
+		if(enableDebugging)
+			Log.d(TAG,"onUnbind");
+		return mAllowRebind;
+	}
+
+	/** Called when a client is binding to the service with bindService()*/
+	@Override
+	public void onRebind(Intent intent) {
+		if(enableDebugging)
+			Log.d(TAG,"onRebind");
+	}
+
+	/** Called when The service is no longer used and is being destroyed */
+	@Override
+	public void onDestroy() {
+		if(enableDebugging)
+			Log.d(TAG,"onDestroy");
+	}
+
 }
