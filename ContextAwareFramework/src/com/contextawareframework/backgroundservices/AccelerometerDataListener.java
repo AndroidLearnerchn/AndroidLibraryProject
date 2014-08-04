b039588a99a1aa5a3ee0e74be0663a5eb44e98cf
androidtestap <androidtestap@gmail.com>
2014-08-04 17:39:35 +0530

    Proper commenting

--- ContextAwareFramework/src/com/contextawareframework/backgroundservices/AccelerometerDataListener.java
+++ ContextAwareFramework/src/com/contextawareframework/backgroundservices/AccelerometerDataListener.java
@@ -1,13 +1,4 @@
 /*
- * 
- * @File        AccelerometerDataListener
- * @Created:    20.11.2013
- * @author:     Prasenjit
- * Last Change: 24.07.2014 by Prasenjit
- */
-/*
- * Copyright (c) 2013 by CDAC Chennai 
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -19,7 +10,13 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @File        AccelerometerDataListener
+ * @Created:    20.11.2013
+ * @author:     Prasenjit
+ * Last Change: 24.07.2014 by Prasenjit
  */
+
 package com.contextawareframework.backgroundservices;
 
 import android.content.Context;
